98d91329d1e98fc04b7d491dff7e80c897b7d53b
Grzegorz Krawczynski <grzegorzkrawczynski@yahoo.com>
2018-10-02 15:49:58 +0200

    models

--- src/main/java/pl/grzegorzkrawczynski/Boot.java
+++ src/main/java/pl/grzegorzkrawczynski/Boot.java
@@ -1,10 +1,31 @@
 package pl.grzegorzkrawczynski;
 
+import pl.grzegorzkrawczynski.model.User;
+
+import java.sql.*;
+import java.util.Random;
+
 public class Boot {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        try(Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/programming_school?useSSL=false", "root", "coderslab")){
+//            PreparedStatement sql = connection.prepareStatement("SELECT  'Hello Database'");
+//            ResultSet rs = sql.executeQuery();
+           /* while(rs.next()) {
+                String hello = rs.getString(1);
+                System.out.println(hello);
+        }*/
 
+            User user = User.findById(connection, 1);
+            user.setPassword("admin" + new Random().nextInt());
+            System.out.println(user);
+
+            User anna = new User("anna", "anna" + new Random().nextInt() +"@gamil.com ", "87654321");
+            anna.save(connection);
+            System.out.println(anna);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- src/main/java/pl/grzegorzkrawczynski/model/Sql.java
+++ src/main/java/pl/grzegorzkrawczynski/model/Sql.java
@@ -1,4 +1,24 @@
 package pl.grzegorzkrawczynski.model;
 
-public class Sql {
+final class Sql {
+
+    private String createUsersTable = "" +
+            "CREATE TABLE users (\n"+
+            "id INT AUTO_INCREMENT PRIMARY KEY,\n"+
+            "username VARCHAR(255) NOT NULL,\n"+
+            "email VARCHAR(255) NOT NULL UNIQUE,\n"+
+            "password VARCHAR(255) NOT NULL\n"+
+            ");";
+
+    private String createExercisesTable = "";
+
+    private String createSolutionsTable = "";
+
+    private String createUserGroupTable = "";
+
+    private Sql(){
+
+    }
+
+
 }
