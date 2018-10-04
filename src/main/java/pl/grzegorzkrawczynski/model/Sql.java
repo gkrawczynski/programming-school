5c40808f3aeeae0e632a1d7b1ab1173087e60705
Grzegorz Krawczynski <grzegorzkrawczynski@yahoo.com>
2018-10-04 15:49:20 +0200

    added sql tables

--- src/main/java/pl/grzegorzkrawczynski/model/Sql.java
+++ src/main/java/pl/grzegorzkrawczynski/model/Sql.java
@@ -10,11 +10,31 @@ final class Sql {
             "password VARCHAR(255) NOT NULL\n"+
             ");";
 
-    private String createExercisesTable = "";
+    private String createExercisesTable = "" +
+            "CREATE TABLE exercise (\n" +
+            "  id INT AUTO_INCREMENT PRIMARY KEY,\n" +
+            "  username VARCHAR(255) NOT NULL,\n" +
+            "  email VARCHAR(255) NOT NULL UNIQUE,\n" +
+            "  password VARCHAR(255) NOT NULL\n" +
+            ");";
 
-    private String createSolutionsTable = "";
+    private String createSolutionsTable = "" +
+            "CREATE TABLE solution (\n" +
+            "  id INT AUTO_INCREMENT PRIMARY KEY,\n" +
+            "  users_id INT,\n" +
+            "  exercise_id INT(11),\n" +
+            "  username VARCHAR(255) NOT NULL,\n" +
+            "  email VARCHAR(255) NOT NULL UNIQUE,\n" +
+            "  password VARCHAR(255) NOT NULL,\n" +
+            "  FOREIGN KEY (users_id) REFERENCES users(id),\n" +
+            "  FOREIGN KEY (exercise_id) REFERENCES exercise(id)\n" +
+            ");";
 
-    private String createUserGroupTable = "";
+    private String createUserGroupTable = "" +
+            "CREATE TABLE user_group (\n" +
+            "  id INT(11) AUTO_INCREMENT PRIMARY KEY,\n" +
+            "  name VARCHAR(255) NOT NULL\n" +
+            ");";
 
     private Sql(){
 
